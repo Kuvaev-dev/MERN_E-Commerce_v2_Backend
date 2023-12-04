7776f67ddf484b3300aa33a74859126199882735
Somewhere <likeaboss13371@gmail.com>
2023-12-04 15:06:03 +0200

    User Login API Added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
+const { generateToken } = require('../config/jwtToken');
 const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
 
-const createUser = async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({ email: email });
     if (!findUser) {
@@ -8,12 +10,26 @@ const createUser = async (req, res) => {
         const newUser = User.create(req.body);
         res.json(newUser);
     } else {
-        // User Already Exists
-        res.json({
-            msg: 'User Already Exists',
-            success: false,
-        });
+        throw new Error('User Already Exists');
     } 
-};
+});
 
-module.exports = { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    // Check if User Exists or Not
+    const findUser = await User.findOne({ email });
+    if (findUser && await findUser.isPasswordMatched(password)) {
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+        });
+    } else {
+        throw new Error('Invalid Credentials');
+    }
+});
+
+module.exports = { createUser, loginUser };
